bfc46fd0ab935cd7b16fedea354833e29e2fff02
Jorge <jorge.sallauco@tecsup.edu.pe>
2024-06-24 13:24:19 -0500

    Initial Commit

---
+++ SallaucoColeccionDeJuegos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SallaucoColeccionDeJuegos
+//
+//  Created by Sallauco Jorge 2 on 24/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
